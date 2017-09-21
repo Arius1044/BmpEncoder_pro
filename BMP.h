f7ef3adacca4d8ac2fab579b3af1ba14e3e7ff6e
StepanLozinsky <31931306+StepanLozinsky@users.noreply.github.com>
2017-09-21 18:37:38 +0300

    Update BMP.h

--- BMP.h
+++ BMP.h
@@ -5,14 +5,21 @@
 class BMP
 {
 private:
+
+	char* Massage;
+	unsigned int Max_size_Massage;
 	BMPINFO File_INFO;
 	RGB **pixels;
 
-	 int Tcode(int byte);
+	int Tcode(int byte);
 
 public:
 	BMP(char* _way_);
 	~BMP();
 	void get_info() const;
+	void DeCoder();
+	void Coder(char* _massage_);
+	void Shifrator();
+
 
 };
